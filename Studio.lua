b5bca904c5923f5570d5151d449741edf4399cad
dekeffer <keffer@tutamail.com>
2026-02-03 11:47:12 +0100

    more things
    I made the base for the prototype. We have to make it good after that.

--- Studio.lua
+++ Studio.lua
@@ -6,15 +6,9 @@ print("studio.lua")
 function scene:create(event)
     local sceneGroup = self.view
 
-    local function listener( event )
-    print( "listener called" )
     local Logo = display.newImage( "assets/Gamestudiologo.jpg", display.contentCenterX, display.contentCenterY )
     sceneGroup:insert(Logo)
-    print( "Delay gelukt" )
     composer.gotoScene("menu")
-    end
-  
-    timer.performWithDelay(1000, listener)
 end
 
 scene:addEventListener("create", scene)
